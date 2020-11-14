8b5e2a8e31e1f1aa5cd5c27233f1966d681c54d2
BuildTools <unconfigured@null.spigotmc.org>
2020-11-14 15:53:05 -0500

    Fix tooltip not hiding properly on mouse exit

--- UI/UI.gd
+++ UI/UI.gd
@@ -9,8 +9,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Hand.connect("show_tooltip", self, "_on_ShowTooltip")
-	$Hand.connect("hide_tooltip", self, "_on_Hidetooltip")
-	print("Connected both signals at UI level")
+	$Hand.connect("hide_tooltip", self, "_on_HideTooltip")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,11 +17,9 @@ func _ready():
 #	pass
 
 func _on_ShowTooltip(tooltip_text):
-	print("UI SHOWING TOOLTIP")
 	$Tooltip.text = tooltip_text
 	$Tooltip.visible = true
 	
 func _on_HideTooltip():
-	print("UI HIDING TOOLTIP")
 	$Tooltip.text = "Hidden"
 	$Tooltip.visible = false
