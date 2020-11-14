2a5678ef8933c40eda0e54d7ac69ec993639c2c8
BuildTools <unconfigured@null.spigotmc.org>
2020-11-14 12:51:01 -0500

    Add basic UI to show/hide hand

--- Deck/Hand.gd
+++ Deck/Hand.gd
@@ -6,10 +6,10 @@ const UICardScene: PackedScene = preload("res://Cards/UICard.tscn")
 export var hand_limit: int = 5
 
 func _ready() -> void:
-	if Utils.is_main_scene(self):
-		add_card(CardsDatabase.create_card_from_data(CardsDatabase.DATA[CardsDatabase.Bunker]))
-		add_card(CardsDatabase.create_card_from_data(CardsDatabase.DATA[CardsDatabase.Bunker]))
-		add_card(CardsDatabase.create_card_from_data(CardsDatabase.DATA[CardsDatabase.Bunker]))
+	visible = false
+	add_card(CardsDatabase.create_card_from_data(CardsDatabase.DATA[CardsDatabase.Bunker]))
+	add_card(CardsDatabase.create_card_from_data(CardsDatabase.DATA[CardsDatabase.Bunker]))
+	add_card(CardsDatabase.create_card_from_data(CardsDatabase.DATA[CardsDatabase.Bunker]))
 
 func add_card(cardData: Card) -> bool:
 	if _is_at_hand_limit():
@@ -23,3 +23,7 @@ func add_card(cardData: Card) -> bool:
 	
 func _is_at_hand_limit() -> bool:
 	return get_child_count() >= hand_limit
+
+# Toggles hand show/hide when button is pressed
+func _on_HandButton_pressed():
+	visible = !visible
