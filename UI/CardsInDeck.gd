392af7734e7f8c490a538169fad859d7946b7e1b
BuildTools <unconfigured@null.spigotmc.org>
2020-11-14 13:12:08 -0500

    Add deck and discard pile counts to hand UI

---
+++ UI/CardsInDeck.gd
@@ -0,0 +1,21 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	text = "Deck: " + str(Deck.drawPile.size())
+	
+
+
+func _on_HandButton_pressed():
+	visible = !visible
